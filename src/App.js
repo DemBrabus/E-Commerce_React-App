8d3c5f8b832aecff27f29539dfe8b938b129e425
dbs-montoya <danmontoya001@gmail.com>
2019-07-19 09:47:12 -0400

    implimented firebase utils, included ability to use authenticated users into our database

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import Header from './Components/Header/Header';
 import HomePage from './Pages/Homepage/HomePage';
 import ShopPage from './Pages/Shop/ShopPage';
 import RegistrationPage from './Pages/SignIn-SignUp/RegistrationPage';
-import { auth } from './Firebase/Firebase.utils';
+import { auth, createUserProfileDocument } from './Firebase/Firebase.utils';
+import { create } from 'domain';
 
 
 class App extends React.Component {
@@ -21,8 +22,23 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id, 
+              ...snapShot.data()
+            }
+          });
+          console.log(this.state)
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
+
     });
   }
 
--- src/Pages/SignIn-SignUp/RegistrationPage.js
+++ src/Pages/SignIn-SignUp/RegistrationPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './RegistrationPage.scss';
 import SignIn from '../../Components/Sign-In/SignIn';
+import SignUp from '../../Components/SignUp-Component/SignUp';
 
 
 
@@ -10,6 +11,7 @@ export default function RegistrationPage() {
     return (
         <div className='RegistrationPage'>
             <SignIn />
+            <SignUp />
         </div>
     )
 }
